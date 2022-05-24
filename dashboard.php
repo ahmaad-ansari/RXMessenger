2f00980dddc621ad54f98b03bc5f65b9564ca6f6
Ahmaad Ansari <88805493+ahmaad-ansari@users.noreply.github.com>
2022-05-24 14:56:43 -0400

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="styles.css" type="text/css">
+    <link rel="icon" href="images/thumbnail.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="" async defer></script>
+</head>
+<body>
+
+    <div class="sidebar">
+        <div class="logo-header">
+            <h2 id="company-name">RX<span class="slim"> Messenger</span></h2>
+            <h4 id="slogan"><i>"Setting reminders just got easier"</i></h4>
+        </div>
+        <div class="user-info">
+            <div id="user-profile">
+                <h1>AA</h1>
+            </div>
+            <h2 id="welcome-message"><span class="slim">Welcome</span> Ahmaad Ansari!</h2>
+        </div>
+
+        <nav>
+            <ul class="dash-links">
+                <li><a id="current-link" href="dashboard.php">Dashboard</a></li>
+                <li><a href="#">Link 2</a></li>
+                <li><a href="#">Link 3</a></li>
+                <li><a href="#">Link 4</a></li>
+                <li><a href="#">Link 5</a></li>
+                <li><a href="#">Link 6</a></li>
+                <li><a href="#">Link 7</a></li>
+                <li><a href="#">Link 8</a></li>
+            </ul>
+        </nav>
+
+        <div id="logout">
+            <button type="button" class="action-buttons">
+                <i class="fa fa-sign-out"></i> Log out
+            </button>
+        </div>
+
+    </div>
+
+    <div class="content">
+        <div class="header">
+
+        </div>
+
+    </div>
+</body>
+</html>
+
